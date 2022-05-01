6dd3060c0b2a3af385f7be39c7256af879c00e46
chunghyang <sungjun0610@gmail.com>
2022-05-01 19:00:34 +0900

    Changes

---
+++ com/Test.py
@@ -0,0 +1,46 @@
+# pip install selenium
+# pip install chromedriver-autoinstaller
+# pip install bs4
+
+from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+
+# 다나와 사이트 검색
+
+options = Options()
+options.add_argument('headless');
+
+
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome(options=options)
+
+driver.implicitly_wait(5)
+
+driver.get('https://search.danawa.com/dsearch.php?query=RTX+3070')
+
+
+
+# bs4 초기화
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+
+
+goods_list = soup.select('li.prod_item.prod_item_top5')
+
+for v in goods_list:
+    if v.find('div', class_='prod_main_info'):
+        # 상품 모델명, 가격, 이미지
+        name = v.select_one('p.prod_name > a').text.strip()
+       # priceHolder = v.find("div",{"class":"top5_price"})
+        price = v.select_one('div.top5_price > em ').text.strip()
+        img_link = v.select_one('div.thumb_image > a > img').get('data-original')
+        if img_link == None:
+            img_link = v.select_one('div.thumb_image > a > img').get('src')
+        print(name," ||| ", price, "원 ||| ", img_link)
+    print()
+
+# 브라우저 종료
+driver.close()
